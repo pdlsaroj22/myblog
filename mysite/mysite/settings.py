69afb09b2d759e21a3768d06d08d6ec3a86fc5fd
pdlsaroj22 <pdlsaroj22@gmail.com>
2017-07-13 07:31:42 +0000

    Added mysql as the database and provided relevant information

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '<pdlsaroj22>$<mysite>',
+        'USER': '<pdlsaroj22>',
+        'PASSWORD': '<dhaitar1>',
+        'HOST': '<pdlsaroj22.mysql.pythonanywhere-services.com>',
     }
 }
 
