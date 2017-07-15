c0614c05a96627dc14cc5101e5f84971d8282e68
Sage Poudel <pdlsaroj22@gmail.com>
2017-07-15 16:52:31 -0500

    Set debug=false in settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i71kk_hcgj0ixn0i_u2$wae3vv$ou$abl%1ib#_6ar8-9n(ny3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
